cf40ea870f58ae5ae9bb6af6d4f48ff1914e5789
SeraphimKa <fandomethor@gmail.com>
2023-05-31 20:51:40 +0300

    Tag search, RSS feed and Vue greetings component!

---
+++ src/components/Greetings.vue
@@ -0,0 +1,41 @@
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface Props {
+    messages: string[]
+}
+
+const { messages } = defineProps<Props>()
+
+const randomMessage = () => {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
+const greeting = ref(messages[0])
+const setGreeting = (message: string) => {
+    greeting.value = message;
+}
+</script>
+
+<template>
+    <div>
+        <h3>{{ greeting }}! Thank you for visiting!</h3>
+        <button @click="setGreeting(randomMessage())">
+            New Greeting
+        </button>
+    </div>
+</template>
+
+<style scoped>
+button {
+    background-image: linear-gradient(to right, #8e2de2, #4a00e0);
+    color: rgb(255, 196, 0);
+    border: brown solid 1px;
+    border-radius: 5px;
+    padding: 2px 5px;
+    cursor: pointer;
+    font-size: 1.2rem;
+    font-weight: 600;
+}
+</style>
---
+++ src/types/post.ts
@@ -0,0 +1,13 @@
+export default interface Post {
+  url: string;
+  frontmatter: {
+    title: string;
+    pubDate: string;
+    author: string;
+    description: string;
+    image: {
+      url: string;
+      alt: string;
+    };
+  };
+}
